1335e9afab2a57bef8fea2139e070d00b123f06d
shuja <shuja.hafez@gmail.com>
2024-05-22 15:30:30 +0530

    install nginx

---
+++ fronten.sh
@@ -0,0 +1,20 @@
+#dnf install nginx -y  &>>/tmp/expense.log
+#systemctl enable nginx &>>/tmp/expense.log
+#systemctl start nginx
+#rm -rf /usr/share/nginx/html/*
+#cp expense.conf /etc/nginx/default.d/expense.conf
+#curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
+#cd /usr/share/nginx/html
+#unzip /tmp/frontend.zip &>>/tmp/expense.log
+#
+#systemctl restart nginx &>>/tmp/expense.log
+#
+
+- name: Plyabook of frontend
+  hosts: all
+  tasks:
+    - name: install nginx
+      ansible.builtin.dnf:
+        name: nginx
+        state: installed
+
