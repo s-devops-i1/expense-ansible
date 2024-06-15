3ceacf29c825fa7efab5ddae9775786d4a4eb3e8
shuja <shuja.hafez@gmail.com>
2024-06-15 16:58:08 +0530

    storing secrets in json files and retrieving back

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
-ansible-playbook get-secrets.yml -e vault_token=$vault_token
-ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e '@secrets.json'
+ansible-playbook get-secrets.yml -e vault_token=$vault_token -e role_name=$component  -e env=$env
+ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e '@secrets.json' -e '@app.json'
