c9ac59c88ade17cd375c61168fac15893644ef7c
shuja <shuja.hafez@gmail.com>
2024-06-21 19:40:50 +0530

    getting secrets by ansible module

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 component=$1
-ansible-playbook get-secrets.yml -e vault_token=$vault_token -e role_name=$component  -e env=$env
-ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e '@~/secrets.json' -e '@~/app.json'
+ansible-playbook get-secrets.yml -e vault_token=$vault_token
+#ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e '@~/secrets.json' -e '@~/app.json'
 #rm -f ~/secrets.json ~/app.json
