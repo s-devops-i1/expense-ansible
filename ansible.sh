8ff33ab923e05e51c0efa64d83eb1978924bf13b
shuja <shuja.hafez@gmail.com>
2024-06-22 17:47:17 +0530

    testing vault secrets

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 component=$1
-ansible-playbook get-secrets.yml -e vault_token=$vault_token -e ansible_user=ec2-user -e ansible_password=DevOps321
-ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e ansible_user=ec2-user -e ansible_password=DevOps321
+ansible-playbook get-secrets.yml -e vault_token=$vault_token
+#ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e ansible_user=ec2-user -e ansible_password=DevOps321
 #rm -f ~/secrets.json ~/app.json
