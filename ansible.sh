1b7c87248e0376e71faddf2d93d0bf0342ce9e2f
shuja <shuja.hafez@gmail.com>
2024-06-22 19:59:49 +0530

    home dir added

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 component=$1
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e role_name=$component  -e env=$env
-ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e '@secrets.json' -e '@apps.json'
-#rm -f ~/secrets.json ~/app.json
+ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e '@~/secrets.json' -e '@~/apps.json'
+rm -f ~/secrets.json ~/apps.json
