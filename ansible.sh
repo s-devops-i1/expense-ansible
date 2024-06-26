10d828d64ed4abc42e65deb57f9d50a2d9c619d6
shuja <shuja.hafez@gmail.com>
2024-06-26 19:47:58 +0530

    removed secrets.json and app.json before getting from vault

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,5 @@
 component=$1
+rm -f ~/secrets.json ~/apps.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e role_name=$component  -e env=$env
 ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e '@~/secrets.json' -e '@~/apps.json'
 rm -f ~/secrets.json ~/apps.json
