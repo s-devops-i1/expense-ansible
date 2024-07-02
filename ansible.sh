1410c791423280b4300f21c9713f3b4403fdebc5
shuja <shuja.hafez@gmail.com>
2024-07-02 10:24:08 +0530

    getting common/single secret approach

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,5 @@
 component=$1
 rm -f ~/*.json
-ansible-playbook -vvv get-secrets.yml -e vault_token=$vault_token -e role_name=$component  -e env=$env
-exit 1
+ansible-playbook get-secrets.yml -e vault_token=$vault_token -e role_name=$component  -e env=$env
 ansible-playbook -i $component-$env.shujathdevops.online, -e role_name=$component  -e env=$env expense-play.yml -e '@~/secrets.json'
 rm -f ~/*.json
